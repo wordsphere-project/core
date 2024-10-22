024abb6578a2ab15e517bf7da67cf5d6d858f17c
Francisco Barrento <francisco.barrento@gmail.com>
2024-10-22 08:06:19 +0100

    added ContentType value objects and the starting to implement the Con… (#50)
    * added ContentType value objects and the starting to implement the ContentTypeRegistry
    
    * Ensure that update is done 5 days after the article was created.

---
+++ src/Domain/ContentManagement/ValueObjects/ContentType.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WordSphere\Core\Domain\ContentManagement\ValueObjects;
+
+class ContentType
+{
+    public function __construct(
+        public readonly string $key,
+        public readonly string $singularName,
+        public readonly string $pluralName,
+        public readonly string $description,
+        public readonly string $icon
+    ) {}
+}
